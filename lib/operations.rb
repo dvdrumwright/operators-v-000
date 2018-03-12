e48fb1a2fb8a97318baad71b3f715986173a7b4f
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-03-12 18:57:40 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,9 @@
 def unsafe?(speed)
  if speed < 40 
    return true 
-   elsif speed > 60
+ elsif speed > 60
    return true 
-# else 50.between?(40, 60)
-#   return false 
-else 
+ else 
   return false 
  end
  end 
